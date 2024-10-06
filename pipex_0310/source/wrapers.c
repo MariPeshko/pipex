fd968f02d405fc38bd96553631d6656b3c93a523
MariPeshko <marunapeshko@gmail.com>
2024-10-06 15:50:49 +0200

    Descriptions. wrapers.c

--- pipex_0310/source/wrapers.c
+++ pipex_0310/source/wrapers.c
@@ -6,15 +6,29 @@
 /*   By: mpeshko <mpeshko@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:30:17 by mpeshko           #+#    #+#             */
-/*   Updated: 2024/06/29 13:54:25 by mpeshko          ###   ########.fr       */
+/*   Updated: 2024/10/06 15:50:02 by mpeshko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/pipex.h"
 #include "libft/libft.h"
-
-// argument 'fd_open' for an argument that is currently opened and in case of
-// duplacating error need to beclosed before exit.
+/**
+ * The wrapers.c file contains three utility functions designed to wrap common
+ * system calls (dup2, fork, and pipe) and provide consistent error handling. 
+*/
+/**
+ * @brief This function is a wrapper around the dup2 system call.
+ * It attempts to duplicate dupfd onto newfd. If dup2 fails, it handles
+ * the error by closing the fd_open file descriptor (if it's not -2), 
+ * printing an error message, and terminating the program.
+ * The function ensures that the original file descriptor (dupfd) is
+ * closed whether the operation succeeds or fails.
+ * 
+ * @param dupfd: The file descriptor to duplicate.
+ * @param newfd: The target file descriptor where dupfd will be duplicated.
+ * @param fd_open: An additional file descriptor that will be closed on error,
+ * unless its value is -2, which means it will be skipped.
+*/
 void	w_dup2(int dupfd, int newfd, int fd_open)
 {
 	if (dup2(dupfd, newfd) == -1)
@@ -29,6 +43,12 @@ void	w_dup2(int dupfd, int newfd, int fd_open)
 		close(dupfd);
 }
 
+/**
+ * @brief This function handles errors during a fork operation.
+ * When fork fails, it prints an error message, closes the file descriptor
+ * "open_fd", closes both ends of a pipe (given by pipe_fd), and exits 
+ * the program.
+*/
 void	w_errfork_close(int open_fd, int *pipe_fd)
 {
 	perror("Fork Error");
@@ -38,6 +58,11 @@ void	w_errfork_close(int open_fd, int *pipe_fd)
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief This function handles errors related to pipe creation.
+ * It closes the provided file descriptor open_fd, prints an error message
+ * related to pipe, and exits the program.
+*/
 void	w_errpipe_close(int open_fd)
 {
 	close(open_fd);
